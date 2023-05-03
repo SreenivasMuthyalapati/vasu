06ada175d0ca08fb0d9bb4e94592ffcb411cc528
Kuyya <sreenivasmuthyalapati@gmail.com>
2023-05-03 08:53:04 +0530

    First commit

---
+++ src/javaPractice/C1.java
@@ -0,0 +1,19 @@
+package javaPractice;
+
+public class C1 {
+
+	public static void main(String[] args) {
+		
+		
+		int[][] a = {{1,4,3},{4,5,6}};
+		
+		for (int i=0; i<=1;i++) {
+			for(int j = 0; j<1; j++) {
+				System.out.println(a[i][j]);
+			}
+			
+		}
+
+	}
+
+}
